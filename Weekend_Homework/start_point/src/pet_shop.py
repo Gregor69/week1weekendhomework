052b8c6a86c0ec4f4f9f2172108aec66c5ff3ce6
Catriona Kelly <catkelly@Catrionas-MacBook-Pro.local>
2021-01-08 16:29:26 +0000

    first commit

---
+++ Weekend_Homework/start_point/src/pet_shop.py
@@ -0,0 +1,9 @@
+# WRITE YOUR FUNCTIONS HERE
+
+def get_pet_shop_name(self):
+    pet_shop_name = ("Camelot of Pets")
+    return pet_shop_name
+
+def get_total_cash(self):
+    total_cash = (1000)
+    return total_cash
